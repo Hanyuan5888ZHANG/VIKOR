560749cde5caa4c8fbefed2b65982cb87e9ce221
Hanyuan5888ZHANG <t330036186@mail.bnbu.edu.cn>
2026-06-08 20:26:17 +0800

    Create __init__.py

---
+++ models/__init__.py
@@ -0,0 +1,5 @@
+"""Models package for the VIKOR repository."""
+
+from .VIKOR import VIKORAnalysis, VIKORResult, print_vikor_table, vikor
+
+__all__ = ["VIKORAnalysis", "VIKORResult", "print_vikor_table", "vikor"]
